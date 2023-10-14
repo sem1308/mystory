d723900dc6248327525c29f3c62eb03b0e408d1d
sem1308 <hshhan1009@gmail.com>
2023-10-14 20:48:40 +0900

    fix: remove default-login page

--- src/main/java/uos/mystory/MystoryApplication.java
+++ src/main/java/uos/mystory/MystoryApplication.java
@@ -2,8 +2,9 @@ package uos.mystory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MystoryApplication {
 
 	public static void main(String[] args) {
