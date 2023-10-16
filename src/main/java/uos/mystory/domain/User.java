4326715b65562eff07f55847223f859095ec50e2
sem1308 <hshhan1009@naver.com>
2023-10-16 19:36:50 +0900

    feat: User 엔티티 클래스에 생성 및 변경 로직 추가
    [생성]
    Setter를 사용하지 않고 Builder를 통해 생성 메소드를 구현하였으며 이때 생성을 생성 메소드로만 이뤄지도록 다른 생성자들은 전부 접근 권한은 protected로 두었다.
    
    [변경]
    이미 영속성 컨텍스트 안에 있는 user 엔티티 객체에 update 메소드를 사용하여 들어온 인자가 null이 아니면 새로운 값으로 변경하는 로직이다.

--- src/main/java/uos/mystory/domain/User.java
+++ src/main/java/uos/mystory/domain/User.java
@@ -1,13 +1,16 @@
 package uos.mystory.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 import uos.mystory.domain.enums.UserRole;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder(access = AccessLevel.PROTECTED) // 생성로직은 오직 User에서만 할 수 있게
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id @GeneratedValue
     @Column(name = "user_id")
@@ -25,11 +28,26 @@ public class User {
     @Column(length = 11)
     private String phoneNum;
 
-    @Column(columnDefinition = "TINYINT")
+    @Column(columnDefinition = "TINYINT default 5")
     private Integer maxNumBlog;
 
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
     private LocalDateTime createdAt;
+
+    //==생성 메소드==//
+    public static User createUser(String userId, String userPw, String nickname, String phoneNum){
+        // TODO: 비밀번호 암호화 or 암호화된 비밀번호 받기
+        return new UserBuilder().userId(userId).userPw(userPw).nickname(nickname).phoneNum(phoneNum)
+                .role(UserRole.M).maxNumBlog(5).createdAt(LocalDateTime.now()).build();
+    }
+
+    //==변경 메소드==//
+    public void update(String userId, String userPw, String nickname, String phoneNum){
+        this.userId = userId == null ? this.userId : userId;
+        this.userPw = userPw == null ? this.userPw : userPw;
+        this.nickname = nickname == null ? this.nickname : nickname;
+        this.phoneNum = phoneNum == null ? this.phoneNum : phoneNum;
+    }
 }
