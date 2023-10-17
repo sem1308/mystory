4037e161c6550c802d27a9a9aa49bb0cc5ed9488
sem1308 <hshhan1009@gmail.com>
2023-10-17 15:38:20 +0900

    feat: 기본 test 코드에 Assertion 패키지 import 추가

--- src/test/java/uos/mystory/MystoryApplicationTests.java
+++ src/test/java/uos/mystory/MystoryApplicationTests.java
@@ -3,6 +3,7 @@ package uos.mystory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
