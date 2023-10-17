15b6828ffd198b6881ecc5b23fdc18ebf4293fe9
sem1308 <hshhan1009@gmail.com>
2023-10-17 15:40:06 +0900

    feat: 비밀번호 암호화를 위한 PasswordEncoder를 bean으로 등록하는 Config class 추가

---
+++ src/main/java/uos/mystory/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package uos.mystory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    // 암호화에 필요한 PasswordEncoder 를 Bean 등록합니다.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
