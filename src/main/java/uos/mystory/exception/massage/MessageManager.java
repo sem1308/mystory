f7f51998c91d62bb0bb8ce6c2bc6ba2216590eb7
sem1308 <hshhan1009@gmail.com>
2023-10-17 15:35:44 +0900

    feat: exception에 메시지를 넣어줄 ResourceBundler 생성

---
+++ src/main/java/uos/mystory/exception/massage/MessageManager.java
@@ -0,0 +1,13 @@
+package uos.mystory.exception.massage;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MessageManager {
+    private static final ResourceBundle messages = ResourceBundle.getBundle("messages");
+
+    public static String getMessage(String key) {
+        // 주어진 키에 해당하는 오류 메시지를 반환합니다.
+        return messages.getString(key);
+    }
+}
