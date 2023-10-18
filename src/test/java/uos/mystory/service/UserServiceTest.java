74b767774267e171b24444c4896cfaf26a896a27
sem1308 <hshhan1009@gmail.com>
2023-10-18 14:50:51 +0900

    feat: user의 로그인 및 정보변경 테스트 작성

--- src/test/java/uos/mystory/service/UserServiceTest.java
+++ src/test/java/uos/mystory/service/UserServiceTest.java
@@ -6,9 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import uos.mystory.domain.User;
 import uos.mystory.dto.mapping.insert.InsertUserDTO;
+import uos.mystory.dto.mapping.update.UpdateUserDTO;
 import uos.mystory.exception.DuplicateUserIdException;
+import uos.mystory.exception.PasswordMismatchException;
+import uos.mystory.exception.ResourceNotFoundException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -46,9 +50,36 @@ public class UserServiceTest {
     @Test
     public void 유저_로그인() throws Exception {
         //given
-        
+        userService.saveUser(new InsertUserDTO("sem1308", "1308", "ddory", "01000000000"));
+
         //when
-        
         //then
+        // 정상 로그인
+        userService.signIn("sem1308", "1308");
+        // 아이디 존재 X
+        assertThrows(ResourceNotFoundException.class, () -> {
+            userService.signIn("sem13081", "1308");
+        });
+        // 비밀번호 불일치
+        assertThrows(PasswordMismatchException.class, () -> {
+            userService.signIn("sem1308", "13081");
+        });
+    }
+    
+    @Test
+    public void 유저_정보변경() throws Exception {
+        //given
+        String updatedNickname = "ddori";
+        String updatedPhoneNum = "01011111111";
+        Long id = userService.saveUser(new InsertUserDTO("sem1308", "1308", "ddory", "01000000000"));
+
+        //when
+        userService.update(new UpdateUserDTO(id,null,updatedNickname,updatedPhoneNum));
+
+        //then
+        User user = userService.getUser(id);
+        assertEquals(user.getNickname(),updatedNickname);
+        assertEquals(user.getPhoneNum(),updatedPhoneNum);
+
     }
 }
