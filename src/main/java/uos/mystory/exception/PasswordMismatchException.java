db6ad4a0ca5066def9edf84be6846ee67a3f55e4
sem1308 <hshhan1009@gmail.com>
2023-10-18 18:19:58 +0900

    fix: message 작성 방식 변경에 따른 매개변수 명 변경

--- src/main/java/uos/mystory/exception/PasswordMismatchException.java
+++ src/main/java/uos/mystory/exception/PasswordMismatchException.java
@@ -3,7 +3,7 @@ package uos.mystory.exception;
 import uos.mystory.exception.massage.MessageManager;
 
 public class PasswordMismatchException extends RuntimeException {
-    private static final String defaultMessage = MessageManager.getMessage("error.user.user_pw.mismatch");
+    private static final String defaultMessage = MessageManager.getMessage("error.mismatch.user.user_pw");
     public PasswordMismatchException(String message) {
         super(message);
     }
