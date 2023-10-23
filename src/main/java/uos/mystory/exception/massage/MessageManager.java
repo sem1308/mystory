e8615ad139307c309da4bd61c812b8519f5376b1
sem1308 <hshhan1009@naver.com>
2023-10-23 22:03:38 +0900

    feat: 공통 메시지에 어떤 클래스에서 발생한 것인지 명시해주는 기능 추가

--- src/main/java/uos/mystory/exception/massage/MessageManager.java
+++ src/main/java/uos/mystory/exception/massage/MessageManager.java
@@ -10,4 +10,9 @@ public class MessageManager {
         // 주어진 키에 해당하는 오류 메시지를 반환합니다.
         return messages.getString(key);
     }
+
+    public static String getMessage(String key, Class c) {
+        // 주어진 키와 클래스에 해당하는 오류 메시지를 반환합니다.
+        return "["+c+"] "+messages.getString(key);
+    }
 }
