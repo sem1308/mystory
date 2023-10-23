8d37e9e14879029a161053f9d62d3543dcd5cc0d
sem1308 <hshhan1009@naver.com>
2023-10-23 17:09:06 +0900

    feat: query dsl을 통한 블로그 목록 얻는 기능 추가 + paging, sort

--- src/main/java/uos/mystory/repository/querydsl/BlogRepositoryCostomImpl.java
+++ src/main/java/uos/mystory/repository/querydsl/BlogRepositoryCostomImpl.java
@@ -1,6 +1,9 @@
 package uos.mystory.repository.querydsl;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -8,18 +11,21 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import uos.mystory.domain.Blog;
 import uos.mystory.domain.QBlog;
 import uos.mystory.domain.QUser;
 import uos.mystory.dto.response.BlogInfoDTO;
 import uos.mystory.dto.response.QBlogInfoDTO;
 import uos.mystory.repository.condition.BlogSearchCondition;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
-public class BlogRepositoryCostomImpl {
+public class BlogRepositoryCostomImpl implements BlogRepositoryCostom {
     private final JPAQueryFactory queryFactory; // 물론 이를 위해서는 빈으로 등록을 해줘야 한다.
     private final QBlog blog = QBlog.blog;
 
@@ -37,13 +43,28 @@ public class BlogRepositoryCostomImpl {
                         userIdEq(condition.userId())
                 )
                 .offset(pageable.getOffset())
-                .limit(pageable.getPageSize());
+                .limit(pageable.getPageSize())
+                .orderBy(getAllOrderSpecifiers(pageable.getSort()));
 
         List<BlogInfoDTO> content = query.fetch();
         long total = content.size();
 
         return new PageImpl<>(content, pageable, total);
     }
+
+    private OrderSpecifier<?>[] getAllOrderSpecifiers(Sort sort) {
+        List<OrderSpecifier<?>> orderSpecifiers = new ArrayList<>();
+        for (Sort.Order order : sort) {
+            String property = order.getProperty(); // 정렬 대상 필드 이름
+            Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC; // 정렬 방향 (ASC 또는 DESC)
+
+            orderSpecifiers.add(new OrderSpecifier(direction, new PathBuilder<>(Blog.class,"blog").get(property)));
+        }
+
+        // 정렬 조건을 결합
+        return orderSpecifiers.toArray(new OrderSpecifier[0]);
+    }
+
 
     private BooleanExpression userIdEq(Long userId) {
         return userId != null ?  QUser.user.id.eq(userId) :null;
