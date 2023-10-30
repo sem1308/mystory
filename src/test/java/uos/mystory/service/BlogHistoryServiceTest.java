7c612c447faa3ee10b25e019a4262837093ab043
sem1308 <hshhan1009@naver.com>
2023-10-30 14:51:35 +0900

    feat: 날짜별 블로그 이력 가져오기 테스트 추가

--- src/test/java/uos/mystory/service/BlogHistoryServiceTest.java
+++ src/test/java/uos/mystory/service/BlogHistoryServiceTest.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 import uos.mystory.domain.enums.VisitedPath;
 import uos.mystory.dto.mapping.select.SelectBlogHistoryDTO;
 import uos.mystory.dto.response.BlogHistoryInfoDTO;
+import uos.mystory.repository.condition.BlogHistorySearchCondition;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,7 +33,7 @@ class BlogHistoryServiceTest extends CategoryServiceTest{
         blogService.getBlogWhenVisit(blog.getId(), VisitedPath.DEVICE);
 
         //when
-        List<SelectBlogHistoryDTO> histories = blogHistoryService.getBlogHistoryInfoDTOsByBlogId(blog.getId());
+        List<SelectBlogHistoryDTO> histories = blogHistoryService.getBlogHistoryInfoDTOs(blog.getId());
 
         //then
         // 리스트의 HistoryRecord 객체의 count 필드를 모두 더한 총합 계산
@@ -52,10 +54,37 @@ class BlogHistoryServiceTest extends CategoryServiceTest{
         blogService.getBlogWhenVisit(blog.getId(), VisitedPath.DEVICE);
 
         //when
-        BlogHistoryInfoDTO histories = blogHistoryService.getBlogHistoriesByBlogId(blog.getId());
+        BlogHistoryInfoDTO histories = blogHistoryService.getBlogHistories(blog.getId());
 
         //then
         System.out.println(histories);
         assertEquals(5, histories.getTotalVisits());
+    }
+
+
+    @Test
+    public void 날짜별_블로그_이력_가져오기() throws Exception {
+        //given
+        blogService.getBlogWhenVisit(blog.getId(), VisitedPath.SEARCH);
+        blogService.getBlogWhenVisit(blog.getId(), VisitedPath.SEARCH);
+        blogService.getBlogWhenVisit(blog.getId(), VisitedPath.DEVICE);
+        blogService.getBlogWhenVisit(blog.getId(), VisitedPath.DEVICE);
+        blogService.getBlogWhenVisit(blog.getId(), VisitedPath.DEVICE);
+
+        LocalDate from = LocalDate.now();
+        LocalDate to = LocalDate.now();
+        LocalDate to2 = LocalDate.now().minusDays(1);
+
+        BlogHistorySearchCondition condition = new BlogHistorySearchCondition(blog.getId(), from, to);
+        BlogHistorySearchCondition condition2 = new BlogHistorySearchCondition(blog.getId(), from, to2);
+
+        //when
+        BlogHistoryInfoDTO histories = blogHistoryService.getBlogHistories(condition);
+        BlogHistoryInfoDTO histories2 = blogHistoryService.getBlogHistories(condition2);
+
+        //then
+        System.out.println(histories);
+        assertEquals(5, histories.getTotalVisits());
+        assertEquals(0, histories2.getTotalVisits());
     }
 }
