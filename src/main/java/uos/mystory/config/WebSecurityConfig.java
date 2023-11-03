429e8dbfa12b9c5acdd553d8272e7ec0d5001d7e
sem1308 <hshhan1009@naver.com>
2023-11-03 21:14:56 +0900

    feat: post를 위한 csrf 관련 기능 disable

--- src/main/java/uos/mystory/config/WebSecurityConfig.java
+++ src/main/java/uos/mystory/config/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package uos.mystory.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,4 +18,11 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
 }
